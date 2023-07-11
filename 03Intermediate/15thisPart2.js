4ea24abb9f8d8adfb9ad9495f960058e73092499
JEEVAN_SKN <saisaranamjeevan@gmail.com>
2023-07-11 09:25:00 +0530

    proceeding to JS DOM

---
+++ 03Intermediate/15thisPart2.js
@@ -0,0 +1,43 @@
+// this -- for all regular functions this points to window object 
+// inside an object it points to the object 
+
+console.log(this);  // in node it points to an empty object
+//in browser it points to window object
+
+var user = {
+    firtsName : "hitesh",
+    courseCount : 4,
+    getCourseCount: function() {  ///method inside a object 
+        console.log(this);   // this points to the whole object itself
+                                // smaein browser with some added proto etc..
+    },
+};
+
+user.getCourseCount(); // invoking the method
+
+// Note that this is not an regular function call , it is invoking a method thru an object 
+//but wwhen we use "this" within jn a regular function call we get the window object in browser 
+                                                         //or global object in node
+
+function sayHello(){
+    console.log("Hello");
+    console.log(this);
+}
+sayHello();
+
+// even if we put an regular function inside an object it still points to the eindow/global object 
+
+var candidate = {
+    firtsName : "hitesh",
+    courseCount : 4,
+    getCourseCount: function() {  ///method inside a object 
+        console.log(this);   
+        function hello(){  //even inside object regular func always points to global/window object
+            console.log("Hi");
+            console.log(this);
+        }
+        hello();
+    },
+};
+
+candidate.getCourseCount();
