941fbadd3563115e9a0dccc3a7dc3785513d2364
JEEVAN_SKN <saisaranamjeevan@gmail.com>
2023-07-16 01:45:02 +0530

    Proceeding to Lexical Scoping

---
+++ 05Intermediate2/04IIFE.js
@@ -0,0 +1,13 @@
+// IIFE stands for Immediately Invoked Function Expression (a design pattern)
+        // also known as Self executing Anonymous Function 
+
+function SayHello(){
+    console.log("Hi my Name is S K N Jeevan")
+}
+
+SayHello();
+
+//IIFE(self executing anonymous) style 
+
+( function(){console.log("hello")})();
+// declaration in 1st ()         //invoking function
