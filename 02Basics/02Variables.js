bf87ae43e82aa14446616b89b088b23d2590fe09
thugperfect <meenamani9444@gmail.com>
2023-09-02 20:38:15 +0530

    till 03Operators

--- 02Basics/02Variables.js
+++ 02Basics/02Variables.js
@@ -16,14 +16,18 @@ var isLoggedInFromFaceBook = false;
 //  fullName = prompt("Enter your name");
 
 // different ways to print variables concatenated with hard coded text
-console.log("The User ID is:", uid);
-console.log("The User Full Name is:" + firstName +' '+ lastName);
-console.log("The User Email is:", email);
+console.log("The User ID is:", uid); //out:The User ID is:abc123
+console.log("The User Full Name is:" + firstName +' '+ lastName);//out:The User Full Name is:Jeevan S K N
+console.log("The User Email is:", email);//out:The User Email is:skn@jeev.com
 
 
 // template strings `  `
 console.log(`  
-    The User Id       : ${uid} //interpolation
+    The User Id       : ${uid}
     The User FullName : ${firstName +' '+ lastName}
-    The User Email    " ${email}
+    The User Email    : ${email}
 `)
+//out:
+// The User Id       : abc123
+// The User FullName : Jeevan S K N
+// The User Email    : skn@jeev.com
