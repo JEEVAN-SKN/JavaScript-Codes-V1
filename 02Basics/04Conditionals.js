a11e3ed92dbbcca1df9a38c2f6d5a4fc8af56f42
thugperfect <meenamani9444@gmail.com>
2023-09-02 21:08:11 +0530

    06 conditionals

--- 02Basics/04Conditionals.js
+++ 02Basics/04Conditionals.js
@@ -2,7 +2,7 @@ var temperature;
 
 //Get data from google
 
-temperature = 20;
+temperature = 19;
 
 // var result = temperature <20;
 // console.log(result);
@@ -15,4 +15,11 @@ if (temperature < 30){
     console.log("It is moderate outside");
 } else {
     console.log("It's really Hot outside");
+
 }
+
+//if temperature=20 out:It is moderate outside
+
+//if temperature=19 out:It's ver cold outside
+//                      It is moderate outside
+
--- 02Basics/06ConditionalLogin.js
+++ 02Basics/06ConditionalLogin.js
@@ -1,6 +1,8 @@
 var email = true;
-var facebook = true;
-var google = false;
+var facebook = false;
+var google = true;
+
+
 
 if(email) {
     console.log("Login Success");
@@ -16,7 +18,38 @@ if(email || facebook || google) { // or operator
     console.log("Login Success");
 }
 
+if(email && facebook && google){ //and operator
+    console.log("all accounts linked");
+}else{
+    if(!email){
+        console.log("Email not linked");
+    }
+    if(!facebook){
+        console.log("Facebook not linked");
+    }
+    if(!google){
+        console.log("Google not linked");
+    }
 
+}
+
+//other way by accessing objects
+
+var accounts = {email,facebook,google}
+
+if(Object.values(accounts).forEach(acc=>
+    acc)){ //checks whether all elements of object are true
+    console.log("all accounts linked");
+}else{
+    for (const [account,linked] of Object.entries(accounts)){ 
+        //saves all accounts into account and values to linked
+        //if any of the linked value is false 
+        // if executes
+        if(!linked){
+            console.log(`${account} is not linked`);
+        }
+    }
+}
 
 
 
