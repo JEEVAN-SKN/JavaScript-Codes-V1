a5e2043df7f2e44e430d16cd07762ad8ad7a9855
thugperfect <meenamani9444@gmail.com>
2023-09-02 21:31:19 +0530

    till 08ConditionalRoles.js

--- 02Basics/08ConditionalRoles.js
+++ 02Basics/08ConditionalRoles.js
@@ -1,25 +1,34 @@
-var user = "admin";
 
-switch(user) {
-    case "admin":
-        console.log("U get full access");
-        break;
-    // case "admin":
-    //     return console.log("U get full access");
+const readline = require("readline")
+const read = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+        //set a question in terminal
+read.question('Enter role :',(user)=>{
+
+    switch(user){
+        case "admin":
+            console.log("U get full access");
+           break; //if no break the subadmin is also into it
+        // case "admin":
+        // return console.log("U get full access");
+            
+        case "subadmin":
+            console.log("acess to create/del courses");
+            break;
+    
+        case "testprep":
+            console.log("access to create/del tests");
+            break;
+    
+        case "user ":
+            console.log("access to content");
+            break; 
+    
+        default :
+            console.log("Trial User");
         
-    case "subadmin":
-        console.log("acess to create/del courses");
-        break;
-
-    case "testprep":
-        console.log("access to create/del tests");
-        break;
-
-    case "user ":
-        console.log("access to content");
-        break; 
-
-    default :
-        console.log("Trial User");
-        
-}
+    }
+    read.close() 
+})
