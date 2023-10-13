c1351d06f1e58514e970b21aa7e5b7c7ab87dedb
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-10-13 10:40:13 +0530

    adding comments

--- 01Hello/script.js
+++ 01Hello/script.js
@@ -9,3 +9,14 @@ console.log("Hello Jeevan");
 //     Browsers have built in engines for execution 
 //     Can Also be done with Js Engines in system - V8, Spider Monkey, Node 
 //             Eg: node script.js ( in specific directory command line)
+
+
+// Note :
+//In our session when we illustrate more than one concepts or example , I may have commented out some code for less 
+        // confusion in console while execution
+    //try commenting in and out each part individually for checking out their individual execution in console
+
+//Also in some example checking out results in browser console is recommended 
+    //for that create a html file and script tag with source to respective js file to be executes 
+        //hoist it in live server which opens the page in browser 
+                //checkout browser console thru developer tools or use shortcut key "ctrl+shift+J" for chrome 
