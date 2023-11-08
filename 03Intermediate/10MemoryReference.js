ff588d5195e1c022397683404e6d935273e0c4da
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-11-08 11:35:33 +0530

    notes added

---
+++ 03Intermediate/10MemoryReference.js
@@ -0,0 +1,21 @@
+let array = [1,2,3,4,5]
+let assignarray = array
+assignarray[1] = "hello"
+console.log(array,assignarray); //output: [ 1, 'hello', 3, 4, 5 ] [ 1, 'hello', 3, 4, 5 ]
+    //when we changed the assignarray , it also changed the original array
+        //because when we assign an array into another array , the new array just points to the memory location of the contetns of the original array
+        //thats y  when we changed the new array it also affect the original array bcs both of them point at the same location
+
+// to avoid this we can use slice operation  or spread operation to copying array
+
+//slice method 
+let newarray = [1,2,4,5]
+let slicearray = newarray.slice()
+slicearray[2] = "hello"
+console.log(newarray,slicearray); // output: [ 1, 2, 4, 5 ] [ 1, 2, 'hello', 5 ]
+let spreadarray = [...newarray]  // spread operator
+spreadarray[2] = "helio"
+console.log(newarray,slicearray,spreadarray); //[ 1, 2, 4, 5 ] [ 1, 2, 'hello', 5 ] [ 1, 2, 'helio', 5 ]
+// so we can use either spread operator or slice method to copy array 
+
+// LIMITATIONS with spread and slice 
